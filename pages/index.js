51a4363513afc96e2267e378348dff7046e9f9cc
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-08-22 17:28:55 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Hom() {
+  return (
+    <div>
+      <h1>Fiap!</h1>
+      <a href="/p2" >Página 2</a>
+    </div>
+  )
+}
---
+++ pages/p2.js
@@ -0,0 +1,8 @@
+export default function P2() {
+  return (
+    <div>
+      <h1>Página 2!</h1>
+      <a href="/" >Home</a>
+    </div>
+  )
+}
