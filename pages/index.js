a6b9ac6a258eff034c06000da30f3d105ffa4d14
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-09-05 14:51:13 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Hom() {
-  return (
-    <div>
-      <h1>Fiap!</h1>
-      <a href="/p2" >Página 2</a>
-    </div>
-  )
-}
--- pages/p2.js
+++
@@ -1,8 +0,0 @@
-export default function P2() {
-  return (
-    <div>
-      <h1>Página 2!</h1>
-      <a href="/" >Home</a>
-    </div>
-  )
-}
