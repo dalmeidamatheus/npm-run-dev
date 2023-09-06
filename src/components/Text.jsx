6d38f2484d181cddc100f7a3b89ced37b172886c
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-09-05 21:06:36 -0300

    Delete src/components/Text.jsx

--- src/components/Text.jsx
+++
@@ -1,7 +0,0 @@
-export default function Title({children}){
-    return(
-        <h2 className="text-xl font-semibold border-yellow-600 border-l-8 m-2 pl-2">
-            {children}
-        </h2>
-    )
-}
