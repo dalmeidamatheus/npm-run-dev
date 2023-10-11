7fd793e301d58dc8bc76c738489ed1fbbd16be2f
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-10-10 22:22:23 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Title from 'src/components/Title.jsx'
 import NavBar from "src/components/NavBar.jsx";
 
 async function carregarNoticias() {
-  const url = "https://newsapi.org/v2/everything?q=choquei&from=2023-09-30&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
+  const url = "https://newsapi.org/v2/everything?q=choquei&from=2023-10-09&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
   const response = await fetch(url)
   const json = await response.json()
   console.log(json)
@@ -11,7 +11,7 @@ async function carregarNoticias() {
   return json.articles
 }
 async function carregarDestaques() {
-  const url2 = "https://newsapi.org/v2/everything?q=greve&from=2023-10-03&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
+  const url2 = "https://newsapi.org/v2/everything?q=influenciador&from=2023-10-09&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
   const response2 = await fetch(url2)
   const json2 = await response2.json()
   console.log(json2)
@@ -19,7 +19,7 @@ async function carregarDestaques() {
   return json2.articles
 }
 async function carregarNovidades() {
-  const url3 = "https://newsapi.org/v2/everything?q=tecnologia&from=2023-10-03&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
+  const url3 = "https://newsapi.org/v2/everything?q=rave-faixa-de-gaza&from=2023-10-09&sortBy=popularity&apiKey=6b2922947e9c469cb6f5535998ef6419"
   const response3 = await fetch(url3)
   const json3 = await response3.json()
   console.log(json3)
